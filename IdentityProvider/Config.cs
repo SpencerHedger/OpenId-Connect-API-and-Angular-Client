da309bbf40ac23e0e8f3463e9b624fc0f90728ab
Spencer Hedger <spencer.hedger@durham.ac.uk>
2019-09-03 12:12:39 +0100

    Add an example resource and client.

---
+++ IdentityProvider/Config.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using IdentityServer4.Models;
+
+namespace IdentityProvider
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("resourceApi", "API Application")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "clientApp",
+ 
+                    // no interactive user, use the clientid/secret for authentication
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+ 
+                    // secret for authentication
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    AllowedScopes = { "resourceApi" }
+                }
+            };
+        }
+    }
+}
