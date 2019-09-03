b9978d152b1ee477d8cdabbc9de199de15c710d9
Spencer Hedger <spencer.hedger@durham.ac.uk>
2019-09-03 12:46:21 +0100

    Add protected controller that lists Claims.

---
+++ ResourceAPI/Controllers/IdentityController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ResourceAPI.Controllers
+{
+    [Route("[controller]")]
+    [Authorize] // Must be authorized to access this.
+    public class IdentityController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            // List the user Claims in JSON format.
+            return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+        }
+    }
+}
